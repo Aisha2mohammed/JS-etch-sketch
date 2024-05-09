15bddbe138475a01df77c2a3bd2030284b91f791
Aisha mohammed <hayunadus111@gmail.com>
2024-05-09 23:23:31 +0300

    modify the creation of squares

--- script.js
+++ script.js
@@ -5,32 +5,40 @@ container.style.display = "flex";
 container.style.flexWrap = "wrap";
 let input;
 let product;
-let i = 1;
 
 btn.addEventListener("click", (input) => {
   input = parseInt(prompt("Insert the number of square you want to create:"));
   if (input > 0 && input <= 100) {
     product = input * input;
-    square(product, input);
+    square(product);
   }
 });
-function square(product, input) {
-  while (i <= product) {
-    if (i == input + 1) {
-      const div = document.createElement("div");
-      div.setAttribute("class", "square");
-      container.appendChild(div);
-      // container.style.display = "flex";
-      // container.style.flexWrap = "wrap";
-      input += input;
-    } else {
-      const div = document.createElement("div");
-      div.setAttribute("class", "square");
-      container.appendChild(div);
-    }
-    i++;
+
+function square(product) {
+  container.innerHTML = ""; // Clear previous grid
+
+  const squareSize = 100 / input; // Calculate the size of each square based on the number of squares
+  for (let i = 0; i <= product; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${squareSize}%`;
+    div.setAttribute("class", "square");
+    container.appendChild(div);
   }
 }
+// function square(product, input) {
+//   while (i <= product) {
+//     if (i == input + 1) {
+//       const div = document.createElement("div");
+//       div.setAttribute("class", "square");
+//       container.appendChild(div);
+//     } else {
+//       const div = document.createElement("div");
+//       div.setAttribute("class", "square");
+//       container.appendChild(div);
+//     }
+//     i++;
+//   }
+// }
 // function square(input) {
 //   for (let i = 1; i <= input; i++) {
 //     for (let j = 1; j <= input; j++) {
