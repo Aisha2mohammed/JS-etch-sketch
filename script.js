9d7daadb4e611be419d6068d969806b8e6811c44
Aisha mohammed <hayunadus111@gmail.com>
2024-05-10 14:38:15 +0300

    MOdify the creation of squares

--- script.js
+++ script.js
@@ -7,19 +7,25 @@ btn.addEventListener("click", (input) => {
   input = parseInt(prompt("Insert the number of square you want to create:"));
   if (input > 0 && input <= 100) {
     product = input * input;
-    square(product);
+    square(input);
   }
 });
 
-function square(product) {
+function square(input) {
   const squareWidth = 1002 / input; // Calculate the width of each square based on the number of squares
   const squareHeight = 1032 / input;
-  for (let i = 1; i <= product; i++) {
-    const div = document.createElement("div");
-    div.style.width = `${squareWidth}px`;
-    div.style.height = `${squareHeight}px`;
-    // div.style.paddingBottom = `${squareSize}%`;
-    div.setAttribute("class", "square");
+  for (let i = 1; i <= input; i++) {
+    const row = document.createElement("div");
+    row.setAttributes("class", "row");
+
+    for (let j = 1; j <= input; j++) {
+      const div = document.createElement("div");
+      div.style.width = `${squareWidth}px`;
+      div.style.height = `${squareHeight}px`;
+      div.setAttribute("class", "square");
+      row.appendChild(div);
+    }
+
     container.appendChild(div);
   }
 }
